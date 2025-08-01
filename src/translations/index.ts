c9d9c0cb348b89302ced23a9cbbcd6726814227e
Jakub Krzyżanowski <jakubkrzyz.kontakt@gmail.com>
2025-08-01 12:10:51 +0200

    fix: update index.ts to see Polish translations

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,10 +1,12 @@
 import { ES_ES } from "./es";
 import { FR_FR } from "./fr";
 import { PT_BR, PT_PT } from "./pt";
+import { PL_PL } from "./pl"
 
 export const defaultTranslations = {
 	"pt-BR": PT_BR,
 	"pt-PT": PT_PT,
 	"es-ES": ES_ES,
 	"fr-FR": FR_FR,
+	"pl-PL": PL_PL
 };
