183dd557df81525a15cd5c97eaba2a6f994fff76
mosaleh <mosaleh@mail.com>
2025-08-04 11:31:57 +0300

    feat(lang): add ar-sa translation

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,10 +1,12 @@
 import { ES_ES } from "./es";
 import { FR_FR } from "./fr";
 import { PT_BR, PT_PT } from "./pt";
+import { AR_SA } from "./ar";
 
 export const defaultTranslations = {
-	"pt-BR": PT_BR,
-	"pt-PT": PT_PT,
-	"es-ES": ES_ES,
-	"fr-FR": FR_FR,
+  "pt-BR": PT_BR,
+  "pt-PT": PT_PT,
+  "es-ES": ES_ES,
+  "fr-FR": FR_FR,
+  "ar-SA": AR_SA,
 };
