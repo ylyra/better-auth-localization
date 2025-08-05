16e78066b969a25c20eaa22f7acd123c3368b3dd
Dom V <54661880+DomVournias@users.noreply.github.com>
2025-08-05 22:34:01 +0300

    Imported el.ts in the defaultTranslations map
    Imported el.ts and linted the file

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,10 +1,11 @@
 import { ES_ES } from "./es";
 import { FR_FR } from "./fr";
 import { PT_BR, PT_PT } from "./pt";
-import { PL_PL } from "./pl"
-import { ID_ID } from "./id"
+import { PL_PL } from "./pl";
+import { ID_ID } from "./id";
 import { AR_SA } from "./ar";
-import { JA_JP } from "./ja"
+import { JA_JP } from "./ja";
+import { EL_GR } from "./el";
 
 export const defaultTranslations = {
   "pt-BR": PT_BR,
@@ -12,7 +13,8 @@ export const defaultTranslations = {
   "es-ES": ES_ES,
   "fr-FR": FR_FR,
   "pl-PL": PL_PL,
-	"id-ID": ID_ID,
+  "id-ID": ID_ID,
   "ar-SA": AR_SA,
-	"ja-JP": JA_JP,
+  "ja-JP": JA_JP,
+  "el-GR": EL_GR
 };
