f19acd8f989b8c297b593983a77ea05da33dac3d
˜Yan <yanlyra3@gmail.com>
2025-10-18 19:32:48 -0300

    refactor: add BetterAuthClientPlugin return type to betterAuthLocalizationClientPlugin

--- src/client.ts
+++ src/client.ts
@@ -3,7 +3,7 @@ import type { localization } from "./index";
 
 type BetterAuthLocalizationPlugin = typeof localization;
 
-export const betterAuthLocalizationClientPlugin = () => {
+export const betterAuthLocalizationClientPlugin = (): BetterAuthClientPlugin => {
 	return {
 		id: "localization",
 		$InferServerPlugin: {} as ReturnType<BetterAuthLocalizationPlugin>,
